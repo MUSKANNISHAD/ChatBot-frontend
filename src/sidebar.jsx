a81b25aecb4d2e66adbed3ea6b2beba34c1084f0
MUSKANNISHAD <mkumari40432@gmail.com>
2026-04-02 15:14:48 +0530

    integrating backend

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar() {
 
   const getAllThreads = async () => {
     try {
-      const response = await fetch("http://localhost:5000/threads");
+      const response = await fetch("http://ec2-13-60-211-68.eu-north-1.compute.amazonaws.com:5000/threads");
       const data = await response.json();
       const filteredData = data.map(thread => ({ threadId: thread.threadId, title: thread.title }));
       setAllThreads(filteredData);
