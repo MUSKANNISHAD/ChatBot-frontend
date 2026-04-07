e0d23c59d47b5be2ebb1853add1d529c765fa50c
MUSKANNISHAD <mkumari40432@gmail.com>
2026-04-07 14:45:36 +0530

    testing

--- src/chat.jsx
+++ src/chat.jsx
@@ -37,7 +37,7 @@ function Chat() {
 
     return (
         <>
-            {newChat && <h1>what is on your mind!</h1>}
+            {newChat && <h1>Hii  Muskan Nishad , whatsupp</h1>}
             <div className="chats">
                 {
                     prevChats?.map((chat, idx) =>
