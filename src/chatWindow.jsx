90c49968b62c5506d7f8b35607e82847c546c492
MUSKANNISHAD <mkumari40432@gmail.com>
2026-04-12 15:43:46 +0530

    Secured Connection

--- src/chatWindow.jsx
+++ src/chatWindow.jsx
@@ -31,7 +31,7 @@ function ChatWindow({ setShowSidebar }) {
         setPrompt("");
 
 
-        const response = await fetch("https://56.228.49.72:5000/chat", {
+        const response = await fetch("https://stayhub-chat.duckdns.org:5000/chat", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -70,7 +70,7 @@ function ChatWindow({ setShowSidebar }) {
 
     const logOut = async () => {
         const token = localStorage.getItem("token");
-        const logout = await fetch("https://56.228.49.72:5000/logout", {
+        const logout = await fetch("https://stayhub-chat.duckdns.org:5000/logout", {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
@@ -101,7 +101,7 @@ function ChatWindow({ setShowSidebar }) {
     const signup = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("https://56.228.49.72:5000/thread/signUp", {
+        const response = await fetch("https://stayhub-chat.duckdns.org:5000/thread/signUp", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -134,7 +134,7 @@ function ChatWindow({ setShowSidebar }) {
     const login = async (e) => {
         e.preventDefault();
 
-        const loginUser = await fetch("https://56.228.49.72:5000/thread/login", {
+        const loginUser = await fetch("https://stayhub-chat.duckdns.org:5000/thread/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
